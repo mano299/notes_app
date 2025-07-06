143389bc15ffeece5e5ec2e1bba75907f9eaf86e
mano299 <mano299ahmed1@gmail.com>
2025-07-06 17:56:53 +0300

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,65 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16.0, top: 24, bottom: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                "Flutter Tips",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, top: 24, bottom: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  "Flutter Tips",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 16,
+                    top: 16,
+                  ),
+                  child: Text(
+                    'This is a subtitle of the note, this is the first',
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: .5),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                trailing: Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.trash,
+                      color: Colors.black,
+                      size: 26,
+                    ),
+                  ),
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 16,
-                  top: 16,
-                ),
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
                 child: Text(
-                  'This is a subtitle of the note, this is the first',
+                  "May 21,2022",
                   style: TextStyle(
                     color: Colors.black.withValues(alpha: .5),
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
                 ),
-              ),
-              trailing: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    FontAwesomeIcons.trash,
-                    color: Colors.black,
-                    size: 26,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24.0),
-              child: Text(
-                "May 21,2022",
-                style: TextStyle(
-                  color: Colors.black.withValues(alpha: .5),
-                  fontSize: 16,
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
