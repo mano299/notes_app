486be3436fd23e0d8e627511bbf95562e57c48a2
mano299 <mano299ahmed1@gmail.com>
2025-07-07 05:40:48 +0300

    add delete for note item

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -51,7 +51,9 @@ class NoteItem extends StatelessWidget {
                 trailing: Padding(
                   padding: const EdgeInsets.only(bottom: 16.0),
                   child: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      note.delete();
+                    },
                     icon: Icon(
                       FontAwesomeIcons.trash,
                       color: Colors.black,
